7bb9ea01cdc2e4a3dc6ce1617fac7509a8ceaff2
frenchcode59 <denisderoullers@gmail.com>
2023-08-29 14:34:08 +0200

    appli netflix flutter

---
+++ netflix_clone/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone/screen/homepage/homepage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Netflix',
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ netflix_clone/lib/screen/homepage/homepage.dart
@@ -0,0 +1,228 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone/models/movie_model.dart';
+import 'package:netflix_clone/screen/movie_screen/movie_screen.dart';
+import 'package:netflix_clone/widget/content_scroll.dart';
+
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+  final int _currentIndex = 0; // Indice de l'onglet actif
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late PageController _pageController;
+  int _currentIndex = 0; // Indice de l'onglet actif
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: 1, viewportFraction: 0.8);
+  }
+
+  _movieSelector(int index) {
+    return AnimatedBuilder(
+      animation: _pageController,
+      builder: (BuildContext context, Widget? widget) {
+        double value = 1;
+        if (_pageController.position.haveDimensions) {
+          value = _pageController.page! - index;
+          value = (1 - (value.abs() * 0.3) + 0.06).clamp(0.0, 1.0);
+        }
+        return SizedBox(
+          height: Curves.easeInOut.transform(value) * 270.0,
+          width: Curves.easeInOut.transform(value) * 400.0,
+          child: widget,
+        );
+      },
+      child: GestureDetector(
+        onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => MovieScreen(movie: movies[index]),
+          ),
+        ),
+        child: Stack(
+          children: <Widget>[
+            Center(
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black54,
+                      offset: Offset(0.0, 4.0),
+                      blurRadius: 10.0,
+                    ),
+                  ],
+                ),
+                child: Center(
+                  child: Hero(
+                    tag: movies[index].imageUrl,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10.0),
+                      child: Image(
+                        image: AssetImage(movies[index].imageUrl),
+                        height: 220.0,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              left: 30.0,
+              bottom: 40.0,
+              child: Container(
+                width: 250.0,
+                child: Text(
+                  movies[index].title.toUpperCase(),
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        elevation: 0.0,
+        centerTitle: true,
+
+        title: const Image(
+          image: AssetImage('assets/images/netflix_logo.png'),
+          height: 70.0,
+          width: 100.0 ,
+        ),
+
+        leading: IconButton(
+          padding: const EdgeInsets.only(left: 30.0),
+          onPressed: () => print('Menu'),
+          icon: const Icon(Icons.menu),
+          iconSize: 30.0,
+          color: Colors.white,
+        ),
+        actions: <Widget>[
+          IconButton(
+            padding: const EdgeInsets.only(right: 20.0),
+            onPressed: () => print('Search'),
+            icon: const Icon(Icons.search),
+            iconSize: 30.0,
+            color: Colors.white,
+          ),
+        ],
+      ),
+      body: ListView(
+        children: <Widget>[
+          Container(
+            height: 280.0,
+            width: double.infinity,
+            child: PageView.builder(
+              controller: _pageController,
+              itemCount: movies.length,
+              itemBuilder: (BuildContext context, int index) {
+                return _movieSelector(index);
+              },
+            ),
+          ),
+          Container(
+            height: 90.0,
+            child: ListView.builder(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0),
+              scrollDirection: Axis.horizontal,
+              itemCount: labels.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  margin: const EdgeInsets.all(10.0),
+                  width: 160.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    gradient: const LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Color(0xFFD45253),
+                        Color(0xFF9E1F28),
+                      ],
+                    ),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Color(0xFF9E1F28),
+                        offset: Offset(0.0, 2.0),
+                        blurRadius: 6.0,
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Text(
+                      labels[index].toUpperCase(),
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.8,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          const SizedBox(height: 20.0),
+          ContentScroll(
+            images: myList,
+            title: 'My List',
+            imageHeight: 250.0,
+            imageWidth: 150.0,
+          ),
+          const SizedBox(height: 10.0),
+          ContentScroll(
+            images: popular,
+            title: 'Popular',
+            imageHeight: 250.0,
+            imageWidth: 150.0,
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index; // Mettez à jour l'onglet actif
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'My List',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Popular',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
